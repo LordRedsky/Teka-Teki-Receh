56b79a842fe5099b383745e0830d6fe154fbcd73
Reza Ahmad <rezaahmadn@gmail.com>
2022-03-25 00:14:05 +0700

    final

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const quiz = [
-    {
+const quiz = [{
         question: `Istri istri apa yang kecil ?`,
         answer: `microwife`
     },
@@ -8,8 +7,8 @@ const quiz = [
         answer: `Gula aren't`
     },
     {
-        question: `Kenapa zombie kalau nyerang barengan ?`,
-        answer: `Karena kalau sendri zomblo`
+        question: `Gang apa yang selalu bikin ibu-ibu kesel ?`,
+        answer: `Gang-guin suaminya`
     },
     {
         question: `Pemain bola mana yang beratnya 3kg ?`,
@@ -20,16 +19,16 @@ const quiz = [
         answer: `Bebek dikunci stang`
     },
     {
-        question: `Hewan apa yang paling panjang ?`,
-        answer: `Ular antri minyak goreng`
+        question: `Siapa presiden yang imut ?`,
+        answer: `Kim Jong Unch`
     },
     {
-        question: `Kenapa anak Babi kalau jalan nunduk-nunduk ?`,
-        answer: `Karena malu ibunya Babi`
+        question: `Penyanyi luar negeri yang suka sepedaan ?`,
+        answer: `Selena Gowes`
     },
     {
         question: `Buah apa yang nggak punya otak ?`,
-        answer: `semuanya`
+        answer: `Semuanya`
     },
     {
         question: `Gajah apa yang belalainya pendek?`,
@@ -42,8 +41,7 @@ const quiz = [
 ]
 
 function questionsAndAnswer() {
-
-    for (let i=0;i<quiz.length;i++){
+    for (let i = 0; i < quiz.length; i++) {
         document.getElementById(`pertanyaan${i+1}`).innerText = quiz[i].question
     }
 }
@@ -54,17 +52,38 @@ questionsAndAnswer()
 let nama;
 let listScoreLeaderboard = []
 let listNameLeaderboard = []
+
+if (localStorage.length !== 0) {
+    listNameLeaderboard = localStorage.Name.split(",")
+    listScoreLeaderboard = localStorage.Score.split(",")
+}
+
 function clickFunc(origin, destination) {
     document.getElementById(`${origin}`).style.display = 'none'
     document.getElementById(`${destination}`).style.display = 'flex'
     nama = document.getElementById('nama').value
-    if (destination === 'home'){
+    if (origin === 'home' && !nama) {
+        document.getElementById(`${origin}`).style.display = 'flex'
+        document.getElementById(`${destination}`).style.display = 'none'
+        if (!name) {
+            alert("Nama tidak boleh kosong!!!")
+        }
+    } else if (origin === 'home' && nama.length > 8) {
+        document.getElementById(`${origin}`).style.display = 'flex'
+        document.getElementById(`${destination}`).style.display = 'none'
+        if (!name) {
+            alert("Nama terlalu panjang!!!")
+        }
+    }
+
+
+    if (destination === 'home') {
         document.getElementById('nama').value = ''
-        for (let i = 1;i<=10;i++){
+        for (let i = 1; i <= 10; i++) {
             document.getElementById(`jawaban${i}`).value = ''
         }
     }
-    if (destination === 'leaderboard'){
+    if (destination === 'leaderboard') {
         document.getElementById('currentScore').innerText = cekJawaban()
         listNameLeaderboard.push(nama)
         listScoreLeaderboard.push(cekJawaban())
@@ -74,19 +93,18 @@ function clickFunc(origin, destination) {
         document.querySelector('.score').innerHTML = ''
         leaderboardRender()
     }
-    
-    
+
 }
 
-function cekJawaban(){
+function cekJawaban() {
 
     let score = 0
-    for (let i = 0; i < quiz.length;i++){
+    for (let i = 0; i < quiz.length; i++) {
         let perSoal = quiz[i]
         let perJawaban = perSoal.answer
         let jawabanUser = document.getElementById(`jawaban${i+1}`).value
-        if (jawabanUser.toLowerCase() === perJawaban.toLowerCase()){
-            score++
+        if (jawabanUser.toLowerCase() === perJawaban.toLowerCase()) {
+            score += 100
         }
     }
     return score
@@ -96,28 +114,28 @@ function cekJawaban(){
 function leaderboardRender() {
     let nameParent = document.querySelector('.name')
     let scoreParent = document.querySelector('.score')
-    
+
     let nameArray = localStorage.Name.split(",")
     let scoreArray = localStorage.Score.split(",")
 
     let temp;
     let temp2;
-    for (let i = 0; i<nameArray.length;i++){
-        for (let j = 0; j<nameArray.length;j++){
-            if (scoreArray[j]<scoreArray[j+1]){
+    for (let i = 0; i < nameArray.length; i++) {
+        for (let j = 0; j < nameArray.length; j++) {
+            if (scoreArray[j] < scoreArray[j + 1]) {
                 temp = scoreArray[j]
-                scoreArray[j] = scoreArray[j+1]
-                scoreArray[j+1] = temp
+                scoreArray[j] = scoreArray[j + 1]
+                scoreArray[j + 1] = temp
 
                 temp2 = nameArray[j]
-                nameArray[j] = nameArray[j+1]
-                nameArray[j+1] = temp2
+                nameArray[j] = nameArray[j + 1]
+                nameArray[j + 1] = temp2
             }
         }
     }
     console.log(scoreArray);
 
-    for (let i = 0; i<nameArray.length;i++){
+    for (let i = 0; i < nameArray.length; i++) {
         let Name = nameArray[i]
         let pemain = document.createElement('h2')
         pemain.innerHTML = Name
@@ -127,8 +145,12 @@ function leaderboardRender() {
         let nilai = document.createElement('h2')
         nilai.innerHTML = Score
         scoreParent.appendChild(nilai)
-        if (i === 4){
+        if (i === 3) {
             break
         }
     }
+}
+
+if (localStorage.length !== 0) {
+    leaderboardRender()
 }
